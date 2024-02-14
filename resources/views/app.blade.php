a1a46b5709ba8a838202cf22f61e5c6d23cf5ec5
jeyanthiran97 <jeyanthiran97@gmail.com>
2024-02-14 17:18:40 +0530

    bootsr

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -30,9 +30,12 @@
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
      integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
      crossorigin=""/>
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   </head>
   <body class="font-montserrat antialiased ">
-    Hi jeya
+    <div>
+        <h1 class="p-5">Hi jeya</h1>
+    </div>
     {{-- @inertia --}}
   </body>
 </html>
